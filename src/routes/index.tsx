8ff137938150a798c9c76fea2cf89246ce5985df
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-23 18:38:02 -0300

    feat: Added general settings router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 
+import { GeneralSettings } from '../pages/GeneralSettings';
 import {Login} from '../pages/Login';
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
+      <Route path="/configuracoes" component={GeneralSettings} />
     </Switch>
   );
 };
