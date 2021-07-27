7269c84a29fc64fb48311e35c455f9e8930b42e1
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-26 23:57:11 -0300

    feat: Using custom router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import { GeneralSettings } from '../pages/GeneralSettings';
 import {Login} from '../pages/Login';
+import { RouterCustom } from './routerCustom';
 
 export function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/configuracoes" component={GeneralSettings} />
+      <RouterCustom isAuthenticated path="/" exact component={Login} />
+      <RouterCustom isPrivate path="/configuracoes" component={GeneralSettings} />
     </Switch>
   );
 };
