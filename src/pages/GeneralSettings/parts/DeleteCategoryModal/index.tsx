89f98d265fbb0eb63938961537f0a44620ff48ae
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-30 16:23:06 -0300

    refactor: Changed param category name

--- src/pages/GeneralSettings/parts/DeleteCategoryModal/index.tsx
+++ src/pages/GeneralSettings/parts/DeleteCategoryModal/index.tsx
@@ -9,15 +9,21 @@ import { Modal } from '../../../../components/Modal';
 import LoadingGif from '.././../../../assets/images/loading.gif';
 import { DeleteCategoryTag } from './styles';
 
+type CategorySetting = {
+  category_id: number;
+  category_description: string;
+  default_price: number | string;
+}
+
 type DeleteCategoryModalProps = {
   setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>; 
   isOpenModal: boolean;
   handleDeleteCategory(data: any): void;
   deleteCategoryLoader: boolean;
-  categoryDescription: string;
+  category: CategorySetting;
 };
 
-export function DeleteCategoryModal({setIsOpenModal, isOpenModal, handleDeleteCategory, deleteCategoryLoader, categoryDescription}: DeleteCategoryModalProps) {
+export function DeleteCategoryModal({setIsOpenModal, isOpenModal, handleDeleteCategory, deleteCategoryLoader, category}: DeleteCategoryModalProps) {
   const changeFormDeleteCategoryRef = useRef<FormHandles>(null);
   return (
     <Modal
@@ -29,7 +35,7 @@ export function DeleteCategoryModal({setIsOpenModal, isOpenModal, handleDeleteCa
         <button onClick={() => setIsOpenModal(false)}><AiOutlineClose /></button>
       </header>
       <Form ref={changeFormDeleteCategoryRef} onSubmit={handleDeleteCategory}>
-        <p>A categoria <strong>{categoryDescription}</strong> será excluída para sempre e não será possível restaurá-la. Você tem certeza?</p>
+        <p>A categoria <strong>{category.category_description}</strong> será excluída para sempre e não será possível restaurá-la. Você tem certeza?</p>
         <footer>
           <Button type="submit">{deleteCategoryLoader ? <img className="imgLoading" src={LoadingGif} alt="loading" /> : <> <BsCheck /> SIM </>}</Button>
           <button type="button" onClick={() => setIsOpenModal(false)} className="buttonCancel">CANCELAR</button>
